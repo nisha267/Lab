64d83a18a964d80bf55491abc77006a37f538483
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-11-18 10:31:25 +0530

    Update AddtwoPositive.java

--- Lab session 17 Nov.2022/AddtwoPositive.java
+++ Lab session 17 Nov.2022/AddtwoPositive.java
@@ -6,27 +6,27 @@ values are both positive, then return the sum of them.
 Under what curcumstances will the finally code block be 
 executed in a try/catch/finally segments.*/
 
-public class AddtwoPositive {
-	public static int addTwoPositive(int x, int y) {
+public class AddtwoPositive {//main class
+	public static int addTwoPositive(int x, int y) {//method to get two numbers
 
-		try {
+		try {//try block for throwing exception
 			if (x < 0 || y < 0) {
 
 				throw new ArithmeticException("Non positive integer sent");
 
 			}
-		} catch (ArithmeticException e) {
+		} catch (ArithmeticException e) {//catch block to print the exception
 			System.out.println(e);
 
 		}
 
-		finally {
+		finally {//finally block to print the message either exception occurs or not
 			System.out.println("The code is executed");
 		}
-		return (x + y);
+		return (x + y);//returning sum of x and y
 	}
 
-	public static void main(String[] args) {
-		addTwoPositive(6, -9);
+	public static void main(String[] args) {//main method
+		addTwoPositive(6, -9);//calling the TwoPositive method and passing two value
 	}
 }
