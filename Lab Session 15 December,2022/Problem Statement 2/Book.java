1409d09540ae1a2a591d99c7541063feed53e0da
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-12-15 13:54:07 +0530

    Update Book.java

--- Lab Session 15 December,2022/Problem Statement 2/Book.java
+++ Lab Session 15 December,2022/Problem Statement 2/Book.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
-public class Book 
+public class Book //book class
 {
 	public static final Collection<? extends String> Book = null;
 	int bookId;//instance variable
@@ -13,23 +13,23 @@ public class Book
 	public class InvalidBookException extends Exception { //created a custom exception 
 		
 	}
-	public Book(int bookId, String title, String author) throws InvalidBookException  {
+	public Book(int bookId, String title, String author) throws InvalidBookException  {//constructor
 		this.bookId=bookId;
 		this.title=title;
 		this.author=author;
 }
-	class BookStore {
+	class BookStore {//class bookstore
 	List<String> a1= new ArrayList<String>(); //creating list
-		void addBook(Book b) {
+		void addBook(Book b) {//method 1
 			a1.addAll(Book); // book details
 		}
-		void searchByTitle(String title)  {
+		void searchByTitle(String title)  {//method 2
 			if (a1.contains(title)) {
 				System.out.println(a1.containsAll(Book));
 			} else 
 				System.out.println("Book NOT found");
 		}
-		void searchByAuthor(String author) {
+		void searchByAuthor(String author) {//method author name
 			if(a1.contains(author)) {
 				System.out.println(a1.containsAll(Book));
 			}else 
