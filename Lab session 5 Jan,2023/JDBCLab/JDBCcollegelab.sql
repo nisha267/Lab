a223944359ec621be46d032be911516edb7ef9b1
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2023-01-05 14:54:50 +0530

    Add files via upload

---
+++ Lab session 5 Jan,2023/JDBCLab/JDBCcollegelab.sql
@@ -0,0 +1,8 @@
+use nisdb;
+create table College(
+id integer primary key ,
+Sname varchar(20) not null,
+dept varchar(20) not null,
+address varchar(10),
+semester varchar(5));
+#drop table college;
